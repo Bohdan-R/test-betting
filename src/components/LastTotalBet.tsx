e5d3a1919351e182ec63dd6dcc1ce659a11781a0
Bohdan Remeniuk <68867425+Bohdan-R@users.noreply.github.com>
2025-06-03 20:45:28 +0300

    fix order

--- src/components/LastTotalBet.tsx
+++ src/components/LastTotalBet.tsx
@@ -7,17 +7,19 @@ type LastTotalBetProps = {
 };
 
 export const LastTotalBet: FC<LastTotalBetProps> = ({ bets }) => {
+  const orderedKeys: (keyof typeof bets)[] = [EBetType.RED, EBetType.BLACK, EBetType.GREEN, EBetType.JOKER];
   return (
     <div className="flex items-center text-sm leading-5 text-white font-bold">
       <span className="mr-2 text-white-shadow3 font-medium">LAST 100</span>
       <div className="w-[235px] flex justify-between">
-        {Object.entries(bets).map((b) => {
-          const amount = b[1].length;
-          const iconType = b[0] === EBetIcon.JOKER ? EBetIcon.JOKER : EBetIcon.SLASH;
-          const color = b[0] === EBetIcon.JOKER ? EColor.PURPLE : (b[0] as EColor);
-          const iconSize = b[0] === EBetIcon.JOKER ? { width: 14, height: 14 } : { width: 12, height: 6 };
+        {orderedKeys.map((key) => {
+          const amount = bets[key].length;
+          const iconType = key === EBetType.JOKER ? EBetIcon.JOKER : EBetIcon.SLASH;
+          const color = key === EBetType.JOKER ? EColor.PURPLE : (key as unknown as EColor);
+          const iconSize = key === EBetType.JOKER ? { width: 14, height: 14 } : { width: 12, height: 6 };
+
           return (
-            <div key={b[0]} className="flex items-center">
+            <div key={key} className="flex items-center">
               <BetBar iconType={iconType} color={color} size={24} iconSize={iconSize} />
               <span className="ml-2">{amount}</span>
             </div>
