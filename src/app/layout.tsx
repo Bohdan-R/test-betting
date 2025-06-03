9fa4fffc577b304d213b97dfea8365a1393fc3b7
Bohdan Remeniuk <68867425+Bohdan-R@users.noreply.github.com>
2025-06-03 20:34:51 +0300

    update metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,12 @@
-import "./globals.css";
-import type { Metadata } from "next";
+import './globals.css';
+import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: "Recipe Finder",
-  description: "Find recipes using Spoonacular API",
+  title: 'Best BET',
+  description: 'Become a millionaire',
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body className="bg-dark1">
